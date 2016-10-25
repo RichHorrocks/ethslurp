9559caac74f66f19927e33f3f891cea3eaab671d
tjayrush <jrush@greathill.com>
2016-10-25 09:41:12 -0400

    Removal of ./src and .email folders

--- src/ethrlp/ethrlp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ETHRLP_H_
-#define _ETHRLP_H_
-
-#include "utillib.h"
-#include "rlp_options.h"
-
-extern CFileExportContext out;
-
-#endif
--- src/summation/summation.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SUMMATION_H_
-#define _SUMMATION_H_
-
-#include "etherlib.h"
-#include "funcMap.h"
-#include "charts.h"
-#include "accumulator.h"
-
-#endif
